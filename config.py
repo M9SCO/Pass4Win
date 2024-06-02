8297b224ec10770e2644f30a99d039d18f9beba5
M_9SCO <chr.grigorii@gmail.com>
2024-06-02 23:23:25 +0600

    chore(config): replace relevant strings to file

---
+++ config.py
@@ -0,0 +1,25 @@
+import pydantic
+import os
+
+
+class Config(pydantic.BaseModel):
+    __resource_path = f"resources{os.sep}"
+    __logging_file = __resource_path + ".log"
+    __db_file = __resource_path + "local.db"
+
+    @property
+    def resource_path(self):
+        return self.__resource_path
+    @property
+    def logging_file(self):
+        return self.__logging_file
+    @property
+    def db_file(self):
+        return self.__db_file
+
+    @property
+    def db_url(self):
+        return "sqlite:///" + self.db_file
+
+
+config = Config()
