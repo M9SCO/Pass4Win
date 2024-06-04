da791e030a2110fa52231558a5b82486049e1e4c
M_9SCO <chr.grigorii@gmail.com>
2024-06-04 17:44:11 +0600

    feature(get_revisions): move functions from tests

--- data/alembic/src/get_revisions.py
+++ data/alembic/src/get_revisions.py
@@ -1,10 +1,12 @@
+import typing
+
 from alembic.script import ScriptDirectory
 
 from data.alembic.src.get_alembic_config import get_alembic_config
 
 
-def get_revisions():
-    config = get_alembic_config()
+def get_revisions(url: typing.Optional[str] = None):
+    config = get_alembic_config(url=url)
 
     revisions_dir = ScriptDirectory.from_config(config)
 
