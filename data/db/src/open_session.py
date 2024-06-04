205cfbec82e0a930732fb717776a183b74c5a6c1
M_9SCO <chr.grigorii@gmail.com>
2024-06-04 17:41:08 +0600

    fix(db): remove async construction

--- data/db/src/open_session.py
+++ data/db/src/open_session.py
@@ -1,19 +1,9 @@
-import logging
-import traceback
-
 from sqlalchemy.orm import sessionmaker
 
 from data.db.src.create_session_maker import create_session_maker
 
 
-async def open_session(session_maker: sessionmaker = None):
-    try:
-        async_session = session_maker or create_session_maker()
-        async with async_session() as session:
-            yield session
-    except:
-        logging.error(traceback.format_exc())
-        await session.rollback()
-        raise
-    finally:
-        await session.close()
+def open_session(session_maker: sessionmaker = None):
+    session = session_maker or create_session_maker()
+    with session() as session:
+        return session
